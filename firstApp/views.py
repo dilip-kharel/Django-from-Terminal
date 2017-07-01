5ed897ab7abfa648c90992f32f1f05fa863b0fa9
dilip-kharel <dilipkharel247@gmail.com>
2017-06-30 23:00:43 -0500

    after writing some Jinja

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>HEY</h2>")
-
+   # return HttpResponse("<h2>HEY</h2>")
+    return render(request, 'firstApp/home.html')
 # Create your views here.
+ 
